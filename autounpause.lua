c9b274d5acd35d92de666271f32be556388da90b
UnhappyAngel <37224950+UnhappyAngel@users.noreply.github.com>
2018-05-19 18:52:27 +0300

    Delete autounpause.lua

--- autounpause.lua
+++
@@ -1,9 +0,0 @@
-local autounpause = {}
-autounpause.optionEnable = Menu.AddOptionBool({"Utility"}, "Auto Unpause", false)
-function autounpause.OnUpdate()
-	if not Heroes.GetLocal() or not Menu.IsEnabled(autounpause.optionEnable) then return end
-	if GameRules.IsPaused() then
-		Engine.ExecuteCommand("dota_pause")
-	end	
-end
-return autounpause
