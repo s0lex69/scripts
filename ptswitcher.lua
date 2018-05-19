64beaba2b5987a301a2cfb871457ae5bc822916c
UnhappyAngel <37224950+UnhappyAngel@users.noreply.github.com>
2018-05-20 00:03:25 +0300

    try to fix

--- ptswitcher.lua
+++ ptswitcher.lua
@@ -1,6 +1,6 @@
 local ptswitch = {}
 ptswitch.optionEnable = Menu.AddOptionBool({"Utility"}, "PT Switcher", false)
-test = false
+test = true
 test2 = false
 hits = {}
 function ptswitch.OnUpdate()
@@ -39,12 +39,14 @@ function ptswitch.OnUpdate()
 	end
 	if #hits > 0 then
 		for i, hit in ipairs(hits) do
+			Log.Write(hit.source)
 			local timing = ((Entity.GetAbsOrigin(hit.source) - Entity.GetAbsOrigin(myHero)):Length2D() - NPC.GetHullRadius(hit.target)) / hit.movespeed
 			if math.floor(timing + hit.starttime,2) == math.floor(GameRules.GetGameTime(),2) then
 				test2 = true				
 			end
 			if math.floor(timing+hit.starttime) ~= math.floor(GameRules.GetGameTime()) and math.floor(timing+hit.starttime) ~= math.floor(GameRules.GetGameTime())+1 then
 				test2 = false
+				table.remove(hits)
 			end	
 		end
 	end	
@@ -55,6 +57,7 @@ function ptswitch.OnProjectile(projectile)
 	if projectile.source == Heroes.GetLocal() then return end
 	if Entity.GetClassName(projectile.source) == "C_DOTA_BaseNPC_Creep_Lane" then return end
 	if projectile.source and projectile.isAttack and projectile.target == Heroes.GetLocal() then
+		Log.Write(Entity.GetClassName(projectile.source))
 		local moveSpeed = projectile.moveSpeed
 		local myProjectedPosition = Entity.GetAbsOrigin(Heroes.GetLocal())
 		local startTime = GameRules.GetGameTime()
@@ -63,23 +66,24 @@ function ptswitch.OnProjectile(projectile)
 	end	
 end
 function ptswitch.OnPrepareUnitOrders(orders)
-	if not Menu.IsEnabled(ptswitch.optionEnable) or not Engine.IsInGame() or not Heroes.GetLocal() or not orders then return end
-	if NPC.HasState(Heroes.GetLocal(), Enum.ModifierState.MODIFIER_STATE_INVISIBLE) then return end
+	if not Menu.IsEnabled(ptswitch.optionEnable) or not Engine.IsInGame() or not Heroes.GetLocal() or not orders then test = true return end
+	if NPC.HasState(Heroes.GetLocal(), Enum.ModifierState.MODIFIER_STATE_INVISIBLE) then test = true return end
 	if orders.order ~= 5 and orders.order ~= 6 and orders.order ~= 7 and orders.order ~= 8 and orders.order ~= 9 then test = true return end 
 	local myHero = Heroes.GetLocal()
 	local primAttribute = Hero.GetPrimaryAttribute(myHero)
 	if primAttribute == 1 then primAttribute = 2 elseif primAttribute == 2 then primAttribute = 1 end
 	local pt = NPC.GetItem(myHero, "item_power_treads", true)
-	if orders.ability and Entity.IsAbility(orders.ability) then
-		test = false
-		if Ability.GetManaCost(orders.ability) <= 0 then return end
-		if NPC.IsStunned(myHero) or not pt then return end
-		if PowerTreads.GetStats(pt) == 0 then 
-			Ability.CastNoTarget(pt)
-		elseif PowerTreads.GetStats(pt) == 2 then
-			Ability.CastNoTarget(pt)
-			Ability.CastNoTarget(pt)
-		end
+	if pt then
+		if orders.ability and Entity.IsAbility(orders.ability) then
+			if Ability.GetManaCost(orders.ability) <= 0 then return end
+			if NPC.IsStunned(myHero) or not pt then return end
+			if PowerTreads.GetStats(pt) == 0 then 
+				Ability.CastNoTarget(pt)
+			elseif PowerTreads.GetStats(pt) == 2 then
+				Ability.CastNoTarget(pt)
+				Ability.CastNoTarget(pt)
+			end
+		end	
 	end
 end
 
