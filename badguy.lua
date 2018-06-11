958e228d796e3aa0e250c28a2f85fab2c6cbcfc4
UnhappyAngel <37224950+UnhappyAngel@users.noreply.github.com>
2018-06-11 19:59:34 +0300

    Added toxic flame

--- badguy.lua
+++ badguy.lua
@@ -3,15 +3,37 @@ badguy.optionEnable = Menu.AddOptionBool({"Utility", "Bad Guy"}, "Enable", false
 badguy.optionEnableAutoFeed = Menu.AddOptionBool({"Utility", "Bad Guy"}, "Auto-Feed", false)
 badguy.optionEnableAutoLaugh = Menu.AddOptionBool({"Utility", "Bad Guy"}, "Auto Laugh", false)
 badguy.optionSliderAutoLaugh = Menu.AddOptionSlider({"Utility", "Bad Guy"}, "Laugh delay", 15,100,15)
+badguy.optionToxicFlame = Menu.AddOptionBool({"Utility", "Bad Guy"}, "Toxic Flame", false)
+badguy.phrase = {
+	"vk.com/cheats_dota",
+	"uc.zone",
+	"Что-то ты слишком изи",
+	"Как два пальца",
+	"Слишком легко для меня"
+}
 lastlaugh = nil
 laughed = false
+aliveHeroes = {}
+function badguy.OnGameStart()
+	aliveHeroes = {}
+	lastlaugh = nil
+	laughed = false
+end
+function badguy.OnGameEnd()
+	aliveHeroes = {}
+	lastlaugh = nil
+	laughed = false
+end
 function badguy.OnUpdate()
-	if not Heroes.GetLocal() or not Menu.IsEnabled(badguy.optionEnable) then lastlaugh = nil return end
+	if not Heroes.GetLocal() or not Menu.IsEnabled(badguy.optionEnable) then lastlaugh = nil aliveHeroes = {} return end
 	if Menu.IsEnabled(badguy.optionEnableAutoFeed) then
 		badguy.autofeed()
 	end
 	if Menu.IsEnabled(badguy.optionEnableAutoLaugh) then
 		badguy.autolaugh()
+	end
+	if Menu.IsEnabled(badguy.optionToxicFlame) then
+		badguy.toxicFlame()
 	end	
 end	
 function badguy.autofeed()
@@ -38,6 +60,28 @@ function badguy.autolaugh()
 		laughed = true
 	else
 		laughed = false	
+	end	
+end
+function badguy.toxicFlame()
+	local myHero = Heroes.GetLocal()
+	for i = 1, Heroes.Count() do
+		local hero = Heroes.Get(i)
+		if Entity.IsAlive(hero) and not aliveHeroes[Hero.GetPlayerID(hero)] then
+			aliveHeroes[Hero.GetPlayerID(hero)] = hero
+		end	
+	end
+	if #aliveHeroes > 0 then
+		for i, hero in pairs(aliveHeroes) do
+			if not Entity.IsAlive(hero) and aliveHeroes[Hero.GetPlayerID(hero)] and not Entity.IsSameTeam(myHero, hero) then
+				for i = 1, Players.Count() do
+					local player = Players.Get(i)
+					if Player.GetPlayerID(player) == Hero.GetPlayerID(hero) then
+						Engine.ExecuteCommand("say "..Player.GetName(player).." "..badguy.phrase[math.random(#badguy.phrase)])
+						aliveHeroes[Hero.GetPlayerID(hero)] = nil
+					end	
+				end
+			end	
+		end	
 	end	
 end
 return badguy
