611fcea3b90113298c2c30818deba7d6fb971bdb
UnhappyAngel <37224950+UnhappyAngel@users.noreply.github.com>
2018-06-30 11:38:21 +0300

    fix

--- wispautotpbase.lua
+++ wispautotpbase.lua
@@ -1,13 +1,32 @@
 local wisp = {}
 wisp.optionEnable = Menu.AddOptionBool({"Hero Specific", "Io"}, "Auto TP After Relocate", false)
 wisp.toggleKey = Menu.AddKeyOption({"Hero Specific", "Io"}, "Toggle Key", Enum.ButtonCode.KEY_NONE)
-font = Renderer.LoadFont("Tahoma", 18, Enum.FontWeight.BOLD)
-toggled1 = false
-base = nil
+local font = Renderer.LoadFont("Tahoma", 18, Enum.FontWeight.BOLD)
+local toggled1 = false
+local base = nil
+local myHero
+local myTeam
+local needInit = true
+local dieTime
+local x,y
+function wisp.Init()
+	myHero = Heroes.GetLocal()
+	myTeam = Entity.GetTeamNum(myHero)
+	if myTeam == 3 then 
+		base = Vector(7264.000000, 6560.000000, 512.000000)
+	else
+		base = Vector(-7317.406250, -6815.406250, 512.000000)
+	end
+	x,y = Renderer.GetScreenSize()
+	x = x * 0.59
+	y = y * 0.8426
+	needInit = false
+end
+function wisp.OnGameStart()
+	needInit = true
+end
 function wisp.OnUpdate() 
 	if not Heroes.GetLocal() or not Menu.IsEnabled(wisp.optionEnable) then return end
-	local myHero = Heroes.GetLocal()
-	local myTeam = Entity.GetTeamNum(myHero)
 	if NPC.GetUnitName(myHero) ~= "npc_dota_hero_wisp" then return end
 	if Menu.IsKeyDownOnce(wisp.toggleKey) then
 		if toggled1 == false then
@@ -26,35 +45,21 @@ function wisp.OnUpdate()
 				tp = NPC.GetItem(myHero, "item_travel_boots_2", true)
 			end
 		end
-		local dieTime = Modifier.GetCreationTime(mod) + 12
-		if myTeam == 3 then 
-			base = Vector(7264.000000, 6560.000000, 512.000000)
-		else
-			base = Vector(-7317.406250, -6815.406250, 512.000000)
+		if not dieTime then
+			dieTime = Modifier.GetCreationTime(mod) + 12
 		end
-		if tp and Ability.IsReady(tp) and dieTime - GameRules.GetGameTime() <= 3 then
+		if tp and Ability.IsReady(tp) and dieTime - GameRules.GetGameTime() <= 2.96 + NetChannel.GetAvgLatency(Enum.Flow.FLOW_OUTGOING) then
 			Ability.CastPosition(tp, base)
 		end
+	else
+		dieTime = nil	
 	end
 end
 function wisp.OnDraw()
 	if not Menu.IsEnabled(wisp.optionEnable) or not Heroes.GetLocal() then return end
 	if NPC.GetUnitName(Heroes.GetLocal()) ~= "npc_dota_hero_wisp" then return end
-	local x, y = Renderer.GetScreenSize()
-	if x == 1920 and y == 1080 then
-		x, y = 1150, 910
-	elseif x== 1600 and y == 900 then
-		x, y = 950, 755
-	elseif x== 1366 and y == 768 then
-		x, y = 805, 643
-	elseif x==1280 and y == 720 then
-		x, y = 752, 600
-	elseif x==1280 and y == 1024 then
-		x, y = 800, 860
-	elseif x==1440 and y == 900 then
-		x, y = 870, 755
-	elseif x== 1680 and y == 1050 then
-		x, y = 1025, 885
+	if needInit then
+		wisp.Init()
 	end
 	if toggled1 then
 		Renderer.SetDrawColor(90, 255, 100)
